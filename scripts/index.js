f225efe9f5702e7c6da06909b523d3faeeee5af6
senya <zernovasenia@gmail.com>
2025-03-05 22:46:20 +0300

    added base functional of cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,21 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+const cardsList = document.querySelector('.places__list');
 
-// @todo: DOM узлы
+function createCard(cardTitle, imageSrc, deleteCard = deleteCard) {
+    const cardElement = cardTemplate.cloneNode(true);
+    cardElement.querySelector('.card__image').src = imageSrc;
+    cardElement.querySelector('.card__title').textContent = cardTitle;
+    cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+    return cardElement;
+}
 
-// @todo: Функция создания карточки
+const deleteCard = (evt) => {
+    const currentCard = evt.target.closest('.card');
+    currentCard.remove();
+}
 
-// @todo: Функция удаления карточки
 
-// @todo: Вывести карточки на страницу
+initialCards.forEach((card)=>{
+    const newCard = createCard(card.name, card.link, deleteCard);
+    cardsList.append(newCard);
+});
