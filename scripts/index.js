92860d21bc5cc5f2959e08fc9fe9f0ba45fc7b54
senya <zernovasenia@gmail.com>
2025-03-21 10:44:32 +0300

    fix index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,21 @@
 const cardTemplate = document.querySelector('#card-template').content;
 const cardsList = document.querySelector('.places__list');
 
-function createCard(cardTitle, imageSrc, deleteCard = deleteCard) {
-    const cardElement = cardTemplate.cloneNode(true);
-    cardElement.querySelector('.card__image').src = imageSrc;
-    cardElement.querySelector('.card__title').textContent = cardTitle;
-    cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+function createCard(card,  deleteCard) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    cardElement.querySelector('.card__image').src = card.link;
+    cardElement.querySelector('.card__image').alt = card.name;
+    cardElement.querySelector('.card__title').textContent = card.name;
+    cardElement.querySelector('.card__delete-button').addEventListener('click', (evt) => deleteCard(evt.target.closest('.card')));
     return cardElement;
 }
 
-const deleteCard = (evt) => {
-    const currentCard = evt.target.closest('.card');
-    currentCard.remove();
+const deleteCard = (cardElement) => {
+    cardElement.remove();
 }
 
 
 initialCards.forEach((card)=>{
-    const newCard = createCard(card.name, card.link, deleteCard);
+    const newCard = createCard(card, deleteCard);
     cardsList.append(newCard);
 });
