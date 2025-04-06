e710e83698904bf996595c87bfba90f8fd31866b
senya <zernovasenia@gmail.com>
2025-04-06 19:28:26 +0300

    add handler for like card and view image in popup

--- src/components/card.js
+++ src/components/card.js
@@ -1,16 +1,24 @@
 const cardTemplate = document.querySelector('#card-template').content;
 
-export function createCard(card,  deleteCard) {
+export function createCard(card,  deleteCard, handleLike, handleImageClick) {
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-    cardElement.querySelector('.card__image').src = card.link;
-    cardElement.querySelector('.card__image').alt = card.name;
+    const cardImage = cardElement.querySelector('.card__image');
+    cardImage.src = card.link;
+    cardImage.alt = card.name;
     cardElement.querySelector('.card__title').textContent = card.name;
     cardElement.querySelector('.card__delete-button').addEventListener('click', (evt) => deleteCard(evt.target.closest('.card')));
+    cardElement.querySelector('.card__like-button').addEventListener('click', handleLike)
+    cardImage.addEventListener('click', handleImageClick)
     return cardElement;
 }
+
 
 export const deleteCard = (cardElement) => {
     cardElement.remove();
 }
 
-
+export function handleLike(evt) {
+    if (evt.target.classList.contains('card__like-button')) {
+        evt.target.classList.toggle('card__like-button_is-active');
+    }
+}
